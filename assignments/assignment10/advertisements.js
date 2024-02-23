bdb70d438b2091518d0d705512f923e976f8b1c5
ZhongshengXD <zl11@email.sc.edu>
2024-02-22 22:06:00 -0500

    assignment10

--- assignments/assignment10/advertisements.js
+++ assignments/assignment10/advertisements.js
@@ -1,10 +1,10 @@
 // Part 1
 let advertisements = [
-    "Sale - Up to 50% off!",
-    "Free shipping on orders over $50",
-    "New arrivals are now in stock",
-    "Sign up for our newsletter and get 10% off",
-    "Exclusive online-only offers"
+    "gardening is good for your health",
+    "gloden hour is the best time to take photos",
+    "mountain lakes are the best places to relax",
+    "small houses are the best places to live",
+    "snow is the best thing to see in the winter"
 ];
 
 let currentAdIndex = 0;
@@ -20,13 +20,14 @@ displayNextAdvertisement();
 
 setInterval(displayNextAdvertisement, 2000);
 
+
 // Part 2 
 let images = [
-    { src: "images/1.png", attribution: "Image by Artist 1 on Freepik", link: "images/1.png" },
-    { src: "images/2.jpg", attribution: "Image by Artist 2 on Freepik", link: "images/2.jpg" },
-    { src: "images/3.jpg", attribution: "Image by Artist 3 on Freepik", link: "images/3.jpg" },
-    { src: "images/4.jpg", attribution: "Image by Artist 4 on Freepik", link: "images/4.jpg" },
-    { src: "images/5.jpg", attribution: "Image by Artist 5 on Freepik", link: "images/5.jpg" },
+    { src: "images/garden.jpg", attribution: "Image by vecstock", link: "https://www.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_40965130.htm#query=landscape&position=0&from_view=keyword&track=sph&uuid=8e520e53-3fb6-4e41-9da7-682c824a94f7" },
+    { src: "images/golden.jpg", attribution: "Image by wirestock", link: "https://www.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_11342065.htm#query=landscape&position=7&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4" },
+    { src: "images/mountain-lake.jpg", attribution: "Image by wirestock", link: "https://www.freepik.com/free-photo/amazing-shot-beautiful-butchart-gardens-brentwood-bay_20496783.htm#query=landscape&position=27&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4" },
+    { src: "images/small-house.jpg", attribution: "Image by wirestock", link: "https://www.freepik.com/free-photo/small-houses-green-field-with-dark-sky_7553929.htm#query=landscape&position=39&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4" },
+    { src: "images/snow.jpg", attribution: "Image by wirestock", link: "https://www.freepik.com/free-photo/beautiful-scenery-lot-leafless-trees-snow-covered-land-during-sunset_10990489.htm#query=landscape&position=38&from_view=keyword&track=sph&uuid=16f8afcf-90c6-4cae-8249-a03fef90c6f4" }
 ];
 
 
